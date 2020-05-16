d844005907e96aab2e51d0310f6b11c947ea0e59
kamalwc <carmichaelkw@gmail.com>
2020-05-16 19:57:57 -0400

    add question and create user readme file

---
+++ USERREADME.md
@@ -0,0 +1,36 @@
+
+    BADGES HERE
+    #  dfd
+
+
+    ## Decription
+    df d
+
+    ## Table of Contents //CREATE HYPER LINKS OUT OF THIS
+        * prih
+        * hehf
+        * 
+        * ihf
+        * h
+        * 
+        
+    
+    ## Installation
+    df d
+
+    ## Usage
+    ss
+
+    ## License
+    undefined
+
+    ## Contributing
+    sss
+
+    ## Tests
+    sssss
+
+    ## Questions
+        * Profile Picture: ss
+        * Github email address: jdsifjsi
+    
--- index.js
+++ index.js
@@ -5,19 +5,105 @@ var fs = require("fs");
 //ask user questions 
 var prompt = inq.prompt([
     {
-        message:"Whats your favorite Manga?",
-        name:"FavManga",
+        message:"Enter your Badge",
+        name:"badge",
+        type:"input"
+    },
+    {
+        message:"Enter the title of your project",
+        name:"title",
+        type:"input"
+    },
+    {
+        message:"Enter your project description",
+        name:"description",
+        type:"input"
+    },
+    {
+        message:"Enter your table of contents separated by space", // separated by space 
+        name:"table",
+        type:"input"
+    },
+    {
+        message:"Enter your steps to installation guide",
+        name:"installation",
+        type:"input"
+    },
+    {
+        message:"Enter your usage description",
+        name:"usage",
+        type:"input"
+    },
+    {
+        message:"Enter your license",
+        name:"License",
+        type:"input"
+    },
+    {
+        message:"Enter your contribution guide",
+        name:"contribute",
+        type:"input"
+    },
+    {
+        message:"Enter your test description",
+        name:"test",
+        type:"input"
+    },
+    {
+        message:"Does your Github acount have a profile picture?",
+        name:"profilePicture",
+        type:"input"
+    },
+    {
+        message:"Enter the email address associated with your Github account",
+        name:"email",
         type:"input"
     }
 ]).then(function(data){
-    console.log(data.FavManga);
+    const tableOfContents = data.table.split(" ");
+    let list = ``;
+    for( x in tableOfContents){
+        list += `* ${tableOfContents[x]}
+        `;
+    }
+
+    const readMe = `
+    BADGES HERE
+    # ${data.title}
+
+
+    ## Decription
+    ${data.description}
+
+    ## Table of Contents //CREATE HYPER LINKS OUT OF THIS
+        ${list}
+    
+    ## Installation
+    ${data.installation}
+
+    ## Usage
+    ${data.usage}
+
+    ## License
+    ${data.license}
+
+    ## Contributing
+    ${data.contribute}
+
+    ## Tests
+    ${data.test}
+
+    ## Questions
+        * Profile Picture: ${data.profilePicture}
+        * Github email address: ${data.email}
+    `;
+  
+    fs.writeFile('USERREADME.md', readMe, (err) => {
+            if (err) throw err;
+            console.log('Readme Updated');
+          });
 })
 
-// generate the readme file 
-// data will be the read me file
-fs.writeFile('message.txt', data, (err) => {
-    if (err) throw err;
-    console.log('The file has been saved!');
-  });
+
 
 
